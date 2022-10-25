740aac9ff95c8e587899acb322d953b69ee11d8e
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2022-10-25 10:57:27 +0500

    class-validator and class-transformer are added and dto is validated

--- src/portfolio/dto/create-portfolioItem.dto.ts
+++ src/portfolio/dto/create-portfolioItem.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
 export class CreatePortfolioItemDto {
+    @IsOptional()
     id?: number;
+
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @IsString()
     desc: string;
 }
--- src/portfolio/dto/update-portfolioItem.dto.ts
+++ src/portfolio/dto/update-portfolioItem.dto.ts
@@ -1,5 +1,15 @@
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
 export class UpdatePortfolioItemDto {
+    @IsOptional()
+    @IsNumber()
     id?: number;
+    
+    @IsOptional()
+    @IsString()
     title?: string;
+
+    @IsOptional()
+    @IsString()
     desc?: string;
 }
