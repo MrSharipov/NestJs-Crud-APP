ab60839306716ccacda4275f72b88fb155b45dc1
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2022-10-25 11:47:03 +0500

    Swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('NextJs CRUD App')
+    .setDescription('The NextJs CRUD App API description')
+    .setVersion('1.0')
+    .addTag('NextsJs Crud')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3003);
 }
 bootstrap();
